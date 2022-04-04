732d91bbc6a07c2f13fc98dd984288a2fc5aa754
chrislaincoubard <101561839+chrislaincoubard@users.noreply.github.com>
2022-04-04 16:54:43 +0200

    Code Cleaning

--- Graphs.py
+++ Graphs.py
@@ -1,14 +1,11 @@
 import json
 import networkx as nx
-import time
 from pyvis.network import Network
 
-# from networkx.drawing.nx_pydot import write_dot
-# import matplotlib.pyplot as plt
-# from graphviz import Source
-
-with open("Test_glycolysis.json", "r") as f:
-    data = json.load(f)
+def read_Json(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data
 
 def create_edges(data):
     edges = []
@@ -34,46 +31,28 @@ def create_nodes_reactions(data):
     return nodes
 
 def nodes_attributes(data):
-    dict ={}
+    dict = {}
     for metabolite in data["metabolites"]:
         dict[metabolite["id"]]=metabolite
     for key in dict:
         del dict[key]["name"]
     return dict
 
-def add_nodes_with_size(data,taille,groupe,Graph):
-    for node in data:
-        Graph.add_node(node, size=taille, group=groupe)
 
-
-def GraphLOL(nodes_r,nodes_m, edges):
-    """ Visualize a directed multigraph as a dot file """
+def Graph_nx(nodes_r,nodes_m, edges):
     G=nx.MultiDiGraph()
-    # add_nodes_with_size(nodes_r,10,2,G)
-    # add_nodes_with_size(nodes_m,5,1,G)
-    G.add_nodes_from(nodes_r,group=1,size=50)
-    G.add_nodes_from(nodes_m,group=2,size=20)
+    G.add_nodes_from(nodes_r,group=1,size=50, title="nodes",shape = "s")
+    G.add_nodes_from(nodes_m,group=2,size=15,title="edges")
     G.add_edges_from(edges)
-    nx.set_node_attributes(G,dict)
+    return G
+
+
+def Graph_pyvis(G):
     nt = Network("750px","750px")
     nt.from_nx(G)
-    nt.add_edges(edges)
-    nt.show_buttons(filter_=['nodes','physics'])
+    # nt.add_edges(edges)
+    nt.show_buttons()
     nt.toggle_hide_edges_on_drag(True)
-    nt.set_edge_smooth("straightCross")
+    nt.set_edge_smooth("cubicBezier")
+    # nt.toggle_stabilization(False)
     nt.show('nx.html')
-
-
-start_time = time.time()
-nodes_m = create_nodes_metabolites(data)
-nodes_r = create_nodes_reactions(data)
-edges = create_edges(data)
-dict = nodes_attributes(data)
-
-
-GraphLOL(nodes_r,nodes_m,edges)
-print("---- %s s -----" % (time.time()-start_time))
-# piece of code (a bit dirty) to get the value of all attribute of nodes of a graph
-# for node in G:
-#     for attribute in G.nodes[node]:
-#         print(f"node : {node} attribute : {attribute} as a value of {G.nodes[node][attribute]}")
