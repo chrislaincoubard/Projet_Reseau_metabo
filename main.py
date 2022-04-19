a638eb9579e44b2ccb297fe2c47ce8ac3d52314a
Kl4ims <102955864+Kl4ims@users.noreply.github.com>
2022-04-19 15:37:25 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,83 @@
+import json
+
+
+with open('Test_glycolysis.json') as json_file:
+    data = json.load(json_file)
+
+    print("Type:", type(data))
+    print(type(data['metabolites']))
+    print("\nmetabolites:", data['metabolites'])
+    print("\nmetabolites:", sorted(data['metabolites'], key=lambda k: k['name']))
+    print("\nreaction:", data['reaction'])
+    print("\nreaction:", sorted(data['reaction'], key=lambda k: k['name']))
+
+
+class Graph:
+    Metabolites = []
+    Reaction = []
+    nodes_metabolites = []
+    nodes_reactions = []
+    edges = []
+
+    def LoadJson(self, file):
+        with open(file, "r") as json_file:
+            self.data = json.load(json_file)
+
+    def __init__(self, file):
+        self.LoadJson(file)
+
+    def print_data(self):
+        print(self.data['metabolites'])
+        print(self.data['reaction'])
+
+    def SortJson(self, file):
+        self.Metabolites.append(sorted(file["metabolites"], key=lambda k: k["name"]))
+        print(self.Metabolites)
+        self.Reaction.append(sorted(file["reaction"], key=lambda k: k["name"]))
+        print(self.Reaction)
+
+    def create_nodes_metabolites(self):
+        for item in self.Metabolites:
+            item["size"] = 20
+            item["group"] = 2
+            item["title"] = item["id"]
+            if "name" in item:
+                del item["name"]
+            self.nodes_metabolites.append((item['id'], item))
+            print(nodes_metabolites)
+
+    def create_nodes_reactions(self):
+        for item in self.Reaction:
+            item["size"] = 50
+            item["group"] = 1
+            item["title"] = item["id"]
+            if "name" in item:
+                del item["name"]
+            self.nodes_reactions.append((item['id'], item))
+            print(nodes_reactions)
+
+    def Search(self, file, category, keyword):
+        a = 0
+        b = len(file)
+        if b == 0:
+            #cas où la liste est vide
+            return False
+        while b > a + 1:
+            m = (a + b) // 2
+            if t[m] > keyword:
+                b = m
+            else:
+                a = m
+        return t[a] == keyword
+
+
+
+g = Graph('Test_glycolysis.json')
+
+g.print_data()
+g.SortJson(data)
+g.create_nodes_metabolites()
+g.create_nodes_reactions()
+
+
+
