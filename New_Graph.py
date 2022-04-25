03c7185aef5df1c0286f6d84a9a09f4fffb47f16
chrislaincoubard <101561839+chrislaincoubard@users.noreply.github.com>
2022-04-25 13:34:16 +0200

    removed unnecessary comments

--- New_Graph.py
+++ New_Graph.py
@@ -107,7 +107,7 @@ class Graph:
         self.create_Graph()
         self.show_graph("My_graph.html")
 
-    def save_graph_json(self, name):  # not working properly still
+    def save_graph_json(self, name): 
         tot_dico = {}
         meta = []
         reac = []
