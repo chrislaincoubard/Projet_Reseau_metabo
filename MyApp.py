5c21eea094314fa271427c1b11d5150ea6601a56
mathildernard <102952292+mathildernard@users.noreply.github.com>
2022-04-30 11:42:10 +0200

    Add files via upload

--- MyApp.py
+++ MyApp.py
@@ -1,6 +1,4 @@
 
-from tkinter.messagebox import YES
-from kivy.uix.button import Button
 from kivy.clock import Clock
 from cgitb import text
 from kivy.app import App
@@ -8,28 +6,29 @@ from kivy.properties import ObjectProperty
 from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.label import Label
-from kivy.uix.tabbedpanel import TabbedPanel, TabbedPanelHeader,TabbedPanelItem
-from kivy.uix.widget import Widget
-from kivy.uix.textinput import TextInput
+from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.boxlayout import BoxLayout
+from functools import partial
+
 import subprocess
 import os
 
 
 
 class MyPanel(TabbedPanel):
-    files=[]
+    files={"gff":"","sbml":"","fna":"","tsv":"","faa1":"","faa2":""}
+    select=False
     format=""
+
+    nomBouton=""
+    clearFiles=False
     module="blast"
     ancien_module=""
     parametre={"i":50,"d":30,"ev":10^100,"c":20,"bs":300}
     defaut={"i":50,"d":30,"ev":10^100,"c":20,"bs":300}
-    compteur_fasta=0
-    compteur_sbml=0
+    
     text_input = ObjectProperty(text)
-    tp = TabbedPanelItem()
-    th = TabbedPanelHeader(text='graph')
-    tp.add_widget(th)
+    
 
     def print_ids(self):
         print(self.ids)
@@ -45,105 +44,75 @@ class MyPanel(TabbedPanel):
     def dismiss_popup_dt(self, dt):
         self._popup.dismiss()
 
-    def show_load(self,text,textbis):
-        self.format=text
+    def show_load(self,text,extension,textbis):
+        self.nomBouton=text
+        self.format=extension
         self.ancien_module=self.module
         self.module=textbis
         if self.module!=self.ancien_module:
-            self.files.clear()
+            for key in self.files.keys():
+                self.files[key] = ""
+
+
         content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
         self._popup = Popup(title="Load file", content=content,
                             size_hint=(0.9, 0.9))
         self._popup.open()
 
+
+
     def load(self, path, filename):
         
         with open(os.path.join(path, filename[0])) as stream:
             self.text_input.text = stream.read()
-
         
-        self.check_format(filename,self.files,self.format)
-        """ print(self.files) """
+        self.check_format(filename)
         
-        self.check_format(filename,self.files,self.format)
         
-    def go_module(self,go_module):
-        if go_module=="blast" and len(self.files)==4:
-            self.launch_blasting(self.files)
-        if go_module=="mpwting" and len(self.files)==3:
+    def go_module(self):
+        check_value=True
+        if self.module=="blast" and self.files.get("faa1")!="" and self.files.get("faa2")!="" and self.files.get("gff")!="" and self.files.get("sbml")!="":
+            if self.files.get("faa1")==self.files.get("faa2"):
+                check_value=False
+            if check_value:
+                self.launch_blasting(self.files)
+        if self.module=="mpwting" and self.files.get("tsv")!="" and self.files.get("fna")!="" and self.files.get("gff")!="":
             self.launch_mpwting(self.files)
-        if go_module=="main" and len(self.files)==6:
-            self.launch_main(self.files)
+        if self.module=="main" :
+            for values in self.files.values():
+                if values=="":
+                    check_value=False
+                if self.files.get("faa1")==self.files.get("faa2"):
+                    check_value=False
+            if check_value:
+                self.launch_main(self.files)
+        
 
          
 
-    def check_format(self, filename,files,format):
-        the_format=False
-        check = filename[0][-4:]
-        compteur=0
-        print(check)
-        name=""
-        if files :
-            for i in files:
-                if i[-4:]==".faa":
-                    compteur+=1
-                    name=i
-                    print("ici")
-                if i[-4:]==format and i[-4:]!=".faa":
-                    self.dismiss_popup()
-                    self._popup = Popup(title='Erreur',content=Label(text='Format deja chargé,reessayez'),size_hint=(0.5,0.5))
-                    self._popup.open()
-                    Clock.schedule_once(self.dismiss_popup_dt, 1)
-                    the_format=False
-                    print("la")
-                if format == ".faa" and compteur>=2:
-                    self.dismiss_popup()
-                    self._popup = Popup(title='Erreur',content=Label(text='Format deja chargé,reessayez'),size_hint=(0.5,0.5))
-                    self._popup.open()
-                    Clock.schedule_once(self.dismiss_popup_dt, 1)
-                    the_format=False
-                    print("et la")
-                else:
-                    the_format=True
+    def check_format(self, filename):
+        extension=filename.split(".")
+        if extension[1] == self.format:
+            for key in self.files.keys():
+                if self.files[key] == self.nomBouton :
+                    if self.files.get(key)!="":
+                        content = ChoiceFiles(cancel=self.dismiss_popup, test =ChoiceFiles.selectchoice)
+                        self._popup = Popup(title="Replace files", content=content,size_hint=(0.4, 0.4))
+                        self._popup.open()
+                    else:    
+                        self.files[key]=filename
+                    if self.select==True:
+                        self.files[key]=filename
+                    
         else:
-            if check == format:      
-                files.append(filename)
-                the_format=False
-                self.dismiss_popup()
-                print("b")
-                self._popup = Popup(title='Information',content=Label(text='Chargement Réussi'),size_hint=(0.5,0.5))
-                self._popup.open()
-                Clock.schedule_once(self.dismiss_popup_dt, 1)
-            else:
-                self._popup = Popup(title='Erreur',content=Label(text='Mauvais Format,reesayez'),size_hint=(0.5,0.5))
-                self._popup.open()
-                Clock.schedule_once(self.dismiss_popup_dt, 1)
+            self._popup = Popup(title='Erreur',content=Label(text='Mauvais Format,reesayez'),size_hint=(0.5,0.5))
+            self._popup.open()
+            Clock.schedule_once(self.dismiss_popup_dt, 1)
+        self.select=False
+            
 
-        if the_format and len(files)!=0:
-            if check == format:
-                if format==".fna" and compteur ==1:
-                    if name==filename:
-                        self._popup = Popup(title='Erreur',content=Label(text='Fichier deja chargé,reesayez'),size_hint=(0.5,0.5))
-                        self._popup.open()
-                        Clock.schedule_once(self.dismiss_popup_dt, 1)
-                    else:
-                        files.append(filename)
-                        print("rela")
-                        self.dismiss_popup()
-                        self._popup = Popup(title='Information',content=Label(text='Chargement Réussi'),size_hint=(0.5,0.5))
-                        self._popup.open()
-                        Clock.schedule_once(self.dismiss_popup_dt, 1)
-                else:
-                    files.append(filename)
-                    self.dismiss_popup()
-                    print("b")
-                    self._popup = Popup(title='Information',content=Label(text='Chargement Réussi'),size_hint=(0.5,0.5))
-                    self._popup.open()
-                    Clock.schedule_once(self.dismiss_popup_dt, 1)
-            else:
-                self._popup = Popup(title='Erreur',content=Label(text='Mauvais Format,reesayez'),size_hint=(0.5,0.5))
-                self._popup.open()
-                Clock.schedule_once(self.dismiss_popup_dt, 1)   
+   
+        
 
 
     
@@ -177,17 +146,17 @@ class MyPanel(TabbedPanel):
             print('command : fail')
         self.files.clear()
     
-    def clear_files(self):
-        self.files.clear()
-        self._popup.dismiss()
+    
 
 
     def show_clear(self):
-        content = Choice(cancel=self.dismiss_popup, test = self.clear_files)
+        content = Choice(cancel=self.dismiss_popup, test = Choice.clear_files)
         self._popup = Popup(title="Clear file", content=content,
                             size_hint=(0.4, 0.4))
         self._popup.open()
-        
+
+
+
     def show_param(self):
         content = Parametres(cancel= self.dismiss_popup)
         self._popup = Popup(title="Enter Parameters", content=content,
@@ -208,30 +177,50 @@ class Choice(FloatLayout):
     cancel = ObjectProperty(None)
     test = ObjectProperty(None)
     
+    def clear_files(self):
+        for key in MyPanel.files.keys():
+            MyPanel.files[key] = ""
+        self._popup = Popup(title='Informations',content=Label(text='Liste de fichiers effacée'),size_hint=(0.5,0.5))
+        self._popup.open()
+
+class ChoiceFiles(FloatLayout):
+    cancel = ObjectProperty(None)
+    test = ObjectProperty(None)
+
+    def selectchoice(self):
+        MyPanel.select=True
+    
+    
+        
+        
+    
 
 class Parametres(BoxLayout):
     
     cancel = ObjectProperty(None)
-    def add_value(self,param):
-        valeur=self.ids['test'].text
-        if valeur=="":
-            MyPanel.parametre[param] = MyPanel.defaut(param)
-        else:
+    def add_value(self,param,id):
+        
+        valeur=self.ids[id].text
+        print(valeur)
+        if valeur!="":
             value=float(valeur)
             if param=="ev" and value<=1 and value>=0:
                 MyPanel.parametre['ev'] = value
-            if param=="i" and value<=100 and value>=0:
+            elif param=="i" and value<=100 and value>=0:
                 MyPanel.parametre['i'] = value
-            if param=="d" and value<=100 and value>=0:
+            elif param=="d" and value<=100 and value>=0:
                 MyPanel.parametre['d'] = value
-            if param=="c" and value<=100 and value>=0:
+            elif param=="c" and value<=100 and value>=0:
                 MyPanel.parametre['c'] = value
-            if param=="bs" and value<=1000 and value>=0:
+            elif param=="bs" and value<=1000 and value>=0:
                 MyPanel.parametre['bs'] = value
             
             else:
                 self._popup = Popup(title='Erreur',content=Label(text="La valeur entrée n'est pas comprise dans l'intervalle "),size_hint=(0.5,0.5))
                 self._popup.open()
+
+    def reset(self):
+        MyPanel.parametre.update(MyPanel.defaut)
             
         
         
