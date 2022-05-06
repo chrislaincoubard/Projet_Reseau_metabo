83ce85243c24397d52ec8e522e5fd4feb4761a3f
mathildernard <102952292+mathildernard@users.noreply.github.com>
2022-05-06 14:44:28 +0200

    final

--- MyApp.py
+++ MyApp.py
@@ -1,7 +1,10 @@
+from io import BufferedRandom
 from kivy.clock import Clock
 from cgitb import text
 from kivy.app import App
 from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
+
 from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from kivy.uix.floatlayout import FloatLayout
@@ -12,28 +15,126 @@ from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.recycleview import RecycleView
-import Graph
+#import Graph
 import subprocess
 import os
 Window.size=(1000,800)
 
 
 class MyPanel(TabbedPanel):
+    
     files={"gff":"","sbml":"","fna":"","tsv":"","faa1":"","faa2":"","json":""}
-    select=False
     format=""
     buttonName=""
-    clearFiles=False
-    module="main"
+    module=""
     old_module=""
-    parametre={"i":50,"d":30,"ev":10^100,"c":20,"bs":300}
-    defaut={"i":50,"d":30,"ev":10^100,"c":20,"bs":300}
+    parametre={"i":50,"d":30,"ev":0,"c":20,"bs":300}
+    defaut={"i":50,"d":30,"ev":0,"c":20,"bs":300}
     filesName=""
     text_input = ObjectProperty(text)
-    verifie_replace=False
-    the_load_check=False
-    graph = Graph.Graph("")
+
     
+    #graph = Graph.Graph("")
+    
+            
+
+    def dismiss_popup(self):
+        self._popup.dismiss()
+
+    def dismiss_popup_dt(self, dt):
+        self._popup1.dismiss()
+
+    def show_load(self,textName,extension,textModule):
+        self.buttonName=textName
+        self.format=extension
+        self.old_module=self.module
+        self.module=textModule
+        if self.module!=self.old_module:
+            for key in self.files.keys():
+                self.files[key] = ""
+        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
+        self._popup = Popup(title="Load file", content=content,
+                            size_hint=(0.9, 0.9))
+        self._popup.open()
+
+    def load(self, path, filename):
+        with open(os.path.join(path, filename[0])) as stream:
+            self.text_input.text = stream.read()
+        self.filesName=filename[0]
+        verifie=self.check_format()
+        if verifie :
+            self.dismiss_popup()
+        extension = filename[0].split(".")
+        if extension[1] == "json":
+            self.graph.load_file(filename[0])
+
+
+    def check_format(self):
+        print("fraiste",self.files)
+        print(self.filesName)
+        print(self.buttonName)
+        good_format=False
+        extension=self.filesName.split(".")
+        if extension[1] == self.format:
+            for key in self.files.keys():
+            
+                if key == self.buttonName :
+                    if self.files.get(key)!="":
+                        self._popup1 = Popup(title="Replace files", content=Label(text="Updated File"),size_hint=(0.4, 0.4))
+                        self._popup1.open()
+                        Clock.schedule_once(self.dismiss_popup_dt, 2)
+                        self.files[key]=self.filesName
+                        good_format=True
+                    else:
+                        self.files[key]=self.filesName
+                        self._popup1 = Popup(title="Replace files", content=Label(text="File successfully loaded ..."),size_hint=(0.4, 0.4))
+                        self._popup1.open()
+                        Clock.schedule_once(self.dismiss_popup_dt, 2)
+                        self.files[key]=self.filesName
+                        good_format=True
+                    
+        else:
+            self._popup1 = Popup(title='Error',content=Label(text='Wrong format, try again'),size_hint=(0.5,0.5))
+            self._popup1.open()
+            Clock.schedule_once(self.dismiss_popup_dt, 1)
+        print(self.files)
+        return (good_format)
+        
+            
+    def go_module(self):
+        check_value=True
+        if self.module=="blast" and self.files.get("faa1")!="" and self.files.get("faa2")!="" and self.files.get("gff")!="" and self.files.get("sbml")!="":
+            if self.files.get("faa1")==self.files.get("faa2"):
+                check_value=False
+            if check_value:
+                self.launch_module()
+        if self.module=="mpwting" and self.files.get("tsv")!="" and self.files.get("fna")!="" and self.files.get("gff")!="":
+            self.launch_module()
+        if self.module=="main" :
+            for values in self.files.values():
+                if values=="":
+                    check_value=False
+                if self.files.get("faa1")==self.files.get("faa2"):
+                    check_value=False
+            if check_value:
+                self.launch_module()
+
+    def launch_module(self):
+        if self.module == "blast":
+            cmd = f"python3 /Users/gauthierdelrot/Documents/M1bioinfo/Projet/Projet_Reseau_metabo-main/blasting.py -n draft -m {self.files['sbml']} -mfaa {self.files['faa1']} -sfaa {self.files['faa2']} -sgff {self.files['gff']} {self.files['sbml']} -i {self.parametre['i']} -d {self.parametre['d']} -ev {self.parametre['ev']} -c {self.parametre['c']} -bs {self.parametre['bs'] }"
+        if self.module == "mpwting":
+            cmd = f"python3 {self.module}.py {self.filesName[0]} {self.filesName[1]} {self.filesName[2]} {self.filesName[3]}"
+        if self.module == "main":
+            cmd = f"python3 {self.module}.py {self.filesName[0]} {self.filesName[1]} {self.filesName[2]} {self.filesName[3]}"
+        print(cmd)
+        p = subprocess.Popen(cmd, shell = True)
+        p.wait()
+        if p.returncode == 0 :
+            print('command : success')
+        else :
+            print('command : fail')
+        self.files.clear()
+
     def print_files(self):
         affiche=[]
         liste_blast=["faa1","faa2","gff","sbml"]
@@ -85,115 +186,6 @@ class MyPanel(TabbedPanel):
     def callback(self,instance):
         if instance.state=="down":
             self.dismiss_popup()
-            
-
-    def dismiss_popup(self):
-        self._popup.dismiss()
-
-    def dismiss_popup_dt(self, dt):
-        self._popup.dismiss()
-
-    def show_load(self,textName,extension,textModule):
-        self.buttonName=textName
-        self.format=extension
-        self.old_module=self.module
-        self.module=textModule
-        if self.module!=self.old_module:
-            for key in self.files.keys():
-                self.files[key] = ""
-
-
-        content = LoadDialog(load=self.load, cancel=self.dismiss_popup)
-        self._popup = Popup(title="Load file", content=content,
-                            size_hint=(0.9, 0.9))
-        self._popup.open()
-
-    def load(self, path, filename):
-        self.the_load_check=False
-        with open(os.path.join(path, filename[0])) as stream:
-            self.text_input.text = stream.read()
-        self.filesName=filename[0]
-        verifie=self.check_format()
-        if verifie or self.verifie_replace:
-            self.dismiss_popup()
-            self.verifie_replace=False
-        extension = filename[0].split(".")
-        if extension[1] == "json":
-            self.graph.load_file(filename[0])
-        
-    def go_module(self):
-        check_value=True
-        if self.module=="blast" and self.files.get("faa1")!="" and self.files.get("faa2")!="" and self.files.get("gff")!="" and self.files.get("sbml")!="":
-            if self.files.get("faa1")==self.files.get("faa2"):
-                check_value=False
-            if check_value:
-                self.launch_blasting(self.files)
-        if self.module=="mpwting" and self.files.get("tsv")!="" and self.files.get("fna")!="" and self.files.get("gff")!="":
-            self.launch_mpwting(self.files)
-        if self.module=="main" :
-            for values in self.files.values():
-                if values=="":
-                    check_value=False
-                if self.files.get("faa1")==self.files.get("faa2"):
-                    check_value=False
-            if check_value:
-                self.launch_main(self.files)
-
-    def check_format(self):
-        good_format=False
-        extension=self.filesName.split(".")
-        if extension[1] == self.format:
-            for key in self.files.keys():
-                if key == self.buttonName :
-                    if self.files.get(key)!="":
-                        content = ChoiceFiles(cancel=self.dismiss_popup, test =ChoiceFiles.selectchoice)
-                        self._popup = Popup(title="Replace files", content=content,size_hint=(0.4, 0.4))
-                        self._popup.open()
-
-                    else:
-                        self.files[key]=self.filesName
-                        good_format=True
-                    
-        else:
-            self._popup = Popup(title='Error',content=Label(text='Wrong format, try again'),size_hint=(0.5,0.5))
-            self._popup.open()
-            Clock.schedule_once(self.dismiss_popup_dt, 1)
-        return(good_format)
-            
-    def changefiles(self):
-        self.files[self.buttonName]=self.filesName
-        self.verifie_replace=True
-    
-    def launch_blasting(self,files):
-        cmd = f"python3 blasting.py {files[0]} {files[1]} {files[2]} {files[3]}"
-        p = subprocess.Popen(cmd, shell = True)
-        p.wait()
-        if p.returncode == 0 :
-            print('command : success')
-        else :
-            print('command : fail')
-        self.files.clear()
-    
-    def launch_mpwting(self,files):
-            cmd = f"python3 mpwting.py {files[0]} {files[1]} {files[2]}"
-            p = subprocess.Popen(cmd, shell = True)
-            p.wait()
-            if p.returncode == 0 :
-                print('command : success')
-            else :
-                print('command : fail')
-            self.files.clear()
-
-    def launch_main(self,files):
-        cmd = f"python3 blasting.py {files[0]} {files[1]} {files[2]} {files[3]} {files[4]} {files[5]} {files[6]}"
-        p = subprocess.Popen(cmd, shell = True)
-        p.wait()
-        if p.returncode == 0 :
-            print('command : success')
-        else :
-            print('command : fail')
-        self.files.clear()
-
 
     def show_param(self):
         content = Parameters(cancel= self.dismiss_popup)
@@ -223,22 +215,15 @@ class MyPanel(TabbedPanel):
 class LoadDialog(FloatLayout):
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
-
-class ChoiceFiles(FloatLayout):
-    cancel = ObjectProperty(None)
-    test = ObjectProperty(None)
-
-    def selectchoice(self):
-        MyPanel.changefiles()
     
 
 class Parameters(BoxLayout):
     cancel = ObjectProperty(None)
 
     def add_value(self,param,id):
-        MyPanel.param=param
-        MyPanel.id=id
+        
         valeur=self.ids[id].text
+        
         try:
             if valeur!="":
                 value=float(valeur)
@@ -258,6 +243,7 @@ class Parameters(BoxLayout):
         except:
             self._popup = Popup(title='Error',content=Label(text="Value is not numeric "),size_hint=(0.5,0.5))
             self._popup.open() 
+   
     def reset(self):
         MyPanel.parametre.update(MyPanel.defaut)
 
